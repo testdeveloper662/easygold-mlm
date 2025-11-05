e5fb777c9fb97f4fddd46c6844366499610f3df6
Ayarn-Kretoss <ayarn.m@kretoss.in>
2025-11-05 12:19:04 +0530

    google doc bugs solved

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const userRouter = express.Router();
+const upload = require("../middleware/multer");
 
 // const authenticateToken = require("../middleware/authentication");
 const CaptureOrder = require("../controller/user/captureOrder");
+const UploadProfileImage = require("../controller/user/uploadProfileImage");
+const UploadLogoImage = require("../controller/user/uploadLogoImage");
 
 // Order
 userRouter.post("/capture-order", CaptureOrder);
+
+// Image uploads
+const uploadProfile = upload("profile").single("image");
+const uploadLogo = upload("logo").single("image");
+
+userRouter.post("/:id/profile-image", uploadProfile, UploadProfileImage);
+userRouter.post("/:id/logo-image", uploadLogo, UploadLogoImage);
 
 module.exports = userRouter;

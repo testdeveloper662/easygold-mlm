6478253ea00bda8eb08aa501fe0295258e244d6b
Rakesh <rakesh.m@kretoss.in>
2026-03-17 19:37:44 +0530

    Implement changes

--- src/controller/broker/referBroker.js
+++ src/controller/broker/referBroker.js
@@ -120,30 +120,16 @@ const ReferBroker = async (req, res) => {
         html: emailData.htmlContent,
       };
 
-      if (language === "de") {
-        attachmentPath = path.join(
-          process.cwd(),
-          "public/uploads/agreements/broker_pdf_de.pdf"
-        );
+      if (language == "de") {
+        attachmentPath = `${process.env.NODE_URL}public/uploads/agreements/broker_pdf_de.pdf`;
       } else {
-        attachmentPath = path.join(
-          process.cwd(),
-          "public/uploads/agreements/broker_pdf_en.pdf"
-        );
+        attachmentPath = `${process.env.NODE_URL}public/uploads/agreements/broker_pdf_en.pdf`;
       }
     }
 
     console.log(attachmentPath, "attachmentPath");
 
-    // await SendEmailHelper(mailOptions.subject, mailOptions.html, mailOptions.to, attachmentPath);
-    if (!fs.existsSync(attachmentPath)) {
-      console.log("❌ File not found:", attachmentPath);
-
-      // fallback without attachment
-      await SendEmailHelper(mailOptions.subject, mailOptions.html, mailOptions.to);
-    } else {
-      await SendEmailHelper(mailOptions.subject, mailOptions.html, mailOptions.to, attachmentPath);
-    }
+    await SendEmailHelper(mailOptions.subject, mailOptions.html, mailOptions.to, attachmentPath);
 
     const invitation = await db.BrokerInvitations.findOne({
       where: {

b821480dbf37d07484bc277a76cfc56633c0915a
Rakesh <rakesh.m@kretoss.in>
2026-03-17 18:56:45 +0530

    Implement changes

--- src/controller/broker/referBroker.js
+++ src/controller/broker/referBroker.js
@@ -123,9 +123,9 @@ const ReferBroker = async (req, res) => {
       } else {
         attachmentPath = `${process.env.NODE_URL}public/uploads/agreements/broker_pdf_en.pdf`;
       }
-
-      console.log(attachmentPath, "attachmentPath");
     }
+
+    console.log(attachmentPath, "attachmentPath");
 
     await SendEmailHelper(mailOptions.subject, mailOptions.html, mailOptions.to, attachmentPath);
 
